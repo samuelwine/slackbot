e253db8805c9db592fdf12927eb1d9a909cf1f56
OllieL <codenation18@Codenation18s-MacBook-Pro.local>
2018-03-08 15:48:11 +0000

    addition to app.js

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ var port = process.env.PORT || 1337;
 app.use(bodyParser.urlencoded({ extended: true }));
  
 // test route
-app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
+app.get('/', function (req, res) { res.status(200).send('HI OLLIE!'); });
  
 app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
+
+app.post('/hello', function (req, res, next) {
+    var userName = req.body.user_name;
+    var botPayload = {
+      text : 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
+    };
+    // Loop otherwise..
+    if (userName !== 'slackbot') {
+      return res.status(200).json(botPayload);
+    } else {
+      return res.status(200).end();
+    }
+  });
